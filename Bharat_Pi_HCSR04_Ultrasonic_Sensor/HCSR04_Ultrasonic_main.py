d2d2eecab0dc07cdea8e329062916b1f6003d033
Snehaaa-C <167289991+Snehaaa-C@users.noreply.github.com>
2024-04-17 13:10:23 +0530

    Update HCSR04_Ultrasonic_main.py

--- Bharat_Pi_HCSR04_Ultrasonic_Sensor/HCSR04_Ultrasonic_main.py
+++ Bharat_Pi_HCSR04_Ultrasonic_Sensor/HCSR04_Ultrasonic_main.py
@@ -1,21 +1,30 @@
-#PIN CONNECTIONS
-#VCC - 3.3V/ 5V
-#GND - GND
-#TRIG - 33 (ANY GPIO)
-#ECHO - 32 (ANY GPIO)
-#LCD  PIN CONNECTIONS
-#SDA AND SCL - SDA AND SCL OF ESP32
-#VCC - 5V
-#GND - GND
-#upload lcd library from Bharapi_lcd_display folder if you are using lcd sensor
+#upload lcd library from Bharapi_lcd_display folder if you are using lcd sensor with Ultrasonic
 
+import machine
 from hcsr04 import HCSR04
+from machine import Pin, SoftI2C
+from lcd_api import LcdApi
+from lcd_i2c import I2cLcd
 from time import sleep
+
+I2C_ADDR = 0x27
+totalRows = 2
+totalColumns = 16
+
+#initializing the I2C method for ESP32
+i2c = SoftI2C(scl=Pin(22), sda=Pin(21), freq=10000)  
+
+#initializing the I2C method for ESP8266
+#i2c = I2C(scl=Pin(5), sda=Pin(4), freq=10000)       
+
+lcd = I2cLcd(i2c, I2C_ADDR, totalRows, totalColumns)
+
 
 # ESP32
 sensor = HCSR04(trigger_pin=33, echo_pin=32, echo_timeout_us=10000)
 
 while True:
+
     distance = sensor.distance_cm()
     print("Distance: ", distance, "cm")
     sleep(3)
@@ -24,7 +33,4 @@ while True:
     lcd.putstr("cm")
     sleep(2)
     lcd.clear()
-  
-
-
 
