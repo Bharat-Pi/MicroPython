511d3d89d5b545e5471ee6eb12ce763e2261640e
Snehaaa-C <167289991+Snehaaa-C@users.noreply.github.com>
2024-06-05 21:35:22 +0530

    Update IR_Sensor.py

--- Bharat_Pi_IR_Sensor/IR_Sensor.py
+++ Bharat_Pi_IR_Sensor/IR_Sensor.py
@@ -1,7 +1,17 @@
+/*************************************************************************
+   PROJECT: Bharat Pi IR sensor Sample Code
+   AUTHOR: Bharat Pi
+   DESC: This script will give you the sample testing of IR sensor, which will do the LED light turn on when any Obstacle detected near the sensor and turn off the led when Obstacle cleared and it will give the respective message for every three second when you run the code
+ 
+   COPYRIGHT: BharatPi @MIT license for usage on Bharat Pi boards
+ *************************************************************************/
+
+
 from machine import Pin
 import time
 
-SENSOR_PIN = 23  # GPIO pin connected to the IR sensor
+# GPIO pin connected to the IR sensor
+SENSOR_PIN = 23 
 
 def setup():
     global ir_sensor
@@ -9,11 +19,12 @@ def setup():
     ir_sensor = Pin(SENSOR_PIN, Pin.IN)
 
 def loop():
+    
     # Read the state of the IR sensor
     sensor_state = ir_sensor.value()
 
     # Check if the sensor state has changed
-    if sensor_state == 1:
+    if sensor_state == 0:
         print("Obstacle detected")
     else:
         print("Obstacle cleared")
@@ -26,6 +37,5 @@ setup()
 
 while True:
     loop()
-
 
 
