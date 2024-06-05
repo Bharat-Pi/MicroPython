be97031054f058ebeba019b2ee0603d833ab3efa
Snehaaa-C <167289991+Snehaaa-C@users.noreply.github.com>
2024-06-05 21:42:37 +0530

    Update BharatPi_Hello_world.py

--- Bharat_Pi_Hello_World_Text/BharatPi_Hello_world.py
+++ Bharat_Pi_Hello_World_Text/BharatPi_Hello_world.py
@@ -1,7 +1,8 @@
 /*************************************************************************
    PROJECT: Bharat Pi Hello World Sample Code
    AUTHOR: Bharat Pi
-   DESC: This script will give you the hello world text when you run the code
+
+   DESC: This script will give you the sample text content of hello world when you run the code.
  
    COPYRIGHT: BharatPi @MIT license for usage on Bharat Pi boards
  *************************************************************************/
