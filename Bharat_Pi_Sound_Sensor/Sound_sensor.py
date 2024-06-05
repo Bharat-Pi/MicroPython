757b10848022b978dc0c3fa8422b03040d18e2df
Snehaaa-C <167289991+Snehaaa-C@users.noreply.github.com>
2024-06-05 22:43:32 +0530

    Update Sound_sensor.py

--- Bharat_Pi_Sound_Sensor/Sound_sensor.py
+++ Bharat_Pi_Sound_Sensor/Sound_sensor.py
@@ -1,9 +1,22 @@
-#PIN CONNECTION
-#AO - 12
-#VCC - 3.3V
-#GND - GND
+/*************************************************************************
+   PROJECT: Bharat Pi Sound Sensor Sample Code
+   AUTHOR: Bharat Pi
+
+   DESC: This script will give you the sample testing of sound sensor,
+   which will detect the surrounding sound
+   and gives the sound value as output when you run the code.
+      #PIN CONNECTIONS
+      #AO - 12
+      #VCC - 3.3V
+      #GND - GND
+ 
+   COPYRIGHT: BharatPi @MIT license for usage on Bharat Pi boards
+ *************************************************************************/
+
+
 
 from machine import ADC
+import time
 
 # Define the analog pin connected to the sound sensor
 sound_pin = 12
@@ -19,9 +32,8 @@ while True:
   print("Sound level:", sound_val)
 
   # You can add logic here to trigger actions based on sound level
-  if sound_val > 120:
+  if sound_val > 150:
     print("Loud sound detected!")
 
   # Adjust delay time as needed
   time.sleep(3)
-
