77538f79e2d05c13b5e52a13b6e97c05418b02a4
Snehaaa-C <167289991+Snehaaa-C@users.noreply.github.com>
2024-06-05 23:20:12 +0530

    Update Touch_sensor.py

--- Bharat_Pi_Touch_Sensor/Touch_sensor.py
+++ Bharat_Pi_Touch_Sensor/Touch_sensor.py
@@ -1,7 +1,19 @@
-#PIN CONNECTIONS
-#GND - GND
-#VCC - 3.3V
-#SIG - 32
+/*************************************************************************
+   PROJECT: Bharat Pi Touch Sensor Sample Code
+   AUTHOR: Bharat Pi
+
+   DESC: This script will give you the sample testing of touch sensor,
+   which will detects/sense the touch and
+   if touch detected prints the message (touch detected),
+   if not then prints the message (no touch detected) when you run the code.
+     #PIN CONNECTIONS
+     #GND - GND
+     #VCC - 3.3V
+     #SIG - 32
+
+   COPYRIGHT: BharatPi @MIT license for usage on Bharat Pi boards
+ *************************************************************************/
+
 
 from machine import Pin
 import time
