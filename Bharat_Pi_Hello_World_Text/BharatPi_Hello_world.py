4113ec6f1f35c4ab82b14950309c1fd42234f9fa
Snehaaa-C <167289991+Snehaaa-C@users.noreply.github.com>
2024-06-05 21:07:17 +0530

    Update BharatPi_Hello_world.py

--- Bharat_Pi_Hello_World_Text/BharatPi_Hello_world.py
+++ Bharat_Pi_Hello_World_Text/BharatPi_Hello_world.py
@@ -1,11 +1,19 @@
-#printing hello world message
+/*************************************************************************
+   PROJECT: Bharat Pi Hello World Sample Code
+   AUTHOR: Bharat Pi
+   DESC: This script will give you the hello world text when you run the code
+ 
+   COPYRIGHT: BharatPi @MIT license for usage on Bharat Pi boards
+ *************************************************************************/
 
+
+import machine
+    
 String="Helloo World"
 print(String)
 
 #OR
  
 print("Helloo World !!!") 
-
 
 
