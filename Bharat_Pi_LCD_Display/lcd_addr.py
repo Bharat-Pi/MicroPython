eb038e3e2880e9041f7bd27c877d72cd041fcdf6
Snehaaa-C <167289991+Snehaaa-C@users.noreply.github.com>
2024-06-06 10:57:53 +0530

    Update lcd_addr.py

--- Bharat_Pi_LCD_Display/lcd_addr.py
+++ Bharat_Pi_LCD_Display/lcd_addr.py
@@ -1,10 +1,11 @@
 #checking the address of LCD
+
 import machine
 
-sdaPIN=machine.Pin(21)  #for ESP32
+sdaPIN=machine.Pin(21)  
 sclPIN=machine.Pin(22)
 
-i2c=machine.I2C(sda=sdaPIN, scl=sclPIN, freq=10000)   
+i2c=machine.SoftI2C(sda=sdaPIN, scl=sclPIN, freq=10000)   
 
 devices = i2c.scan()
 if len(devices) == 0:
