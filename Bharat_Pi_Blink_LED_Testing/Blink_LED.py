54557f926fbd282225751a8a2f6dadee9b524dfd
Snehaaa-C <167289991+Snehaaa-C@users.noreply.github.com>
2024-06-06 12:27:20 +0530

    Update Blink_LED.py

--- Bharat_Pi_Blink_LED_Testing/Blink_LED.py
+++ Bharat_Pi_Blink_LED_Testing/Blink_LED.py
@@ -7,6 +7,27 @@
  
    COPYRIGHT: BharatPi @MIT license for usage on Bharat Pi boards
  *************************************************************************/
+      /*************************************************************************
+   PROJECT NAME: Bharat Pi Blink LED Sample Code
+   AUTHOR: Bharat Pi
+   CREATED DATE: 25/03/2024
+   COPYRIGHT: BharatPi @MIT license for usage on Bharat Pi boards
+   VERSION: 0.1.1
+
+   DESCRIPTION: This script will give you the sample testing of blink led,
+   which will do the LED light turn on and turn off for every one second when you run the code.
+
+   REVISION HISTORY TABLE:
+   ------------------------------------------
+   Date      | Firmware Version | Comments
+   ------------------------------------------
+   25/03/2024 -    0.1.0       -    Initial release of the sample script to test led blink(Used PyCraft IDE version - 1.1).
+   06/06/2024 -    0.1.1       -    Added the altered code for Thonny ide(Used Thonny IDE version - 4.1.4).
+
+ *************************************************************************/
+
+
+      
 
       
 import machine
