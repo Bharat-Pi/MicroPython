c743e155ff4d395594a4c0efad04fec6821f1fa5
Snehaaa-C <167289991+Snehaaa-C@users.noreply.github.com>
2024-06-06 13:55:08 +0530

    Update Sound_sensor.py

--- Bharat_Pi_Sound_Sensor/Sound_sensor.py
+++ Bharat_Pi_Sound_Sensor/Sound_sensor.py
@@ -1,20 +1,28 @@
 /*************************************************************************
-   PROJECT: Bharat Pi Sound Sensor Sample Code
+   
+   PROJECT NAME: Bharat Pi Sound Sensor Sample Code
    AUTHOR: Bharat Pi
+   CREATED DATE: 25/03/2024
+   COPYRIGHT: BharatPi @MIT license for usage on Bharat Pi boards
+   VERSION: 0.1.1
 
-   DESC: This script will give you the sample testing of sound sensor,
+   DESCRIPTION: This script will give you the sample testing of sound sensor,
    which will detect the surrounding sound
    and gives the sound value as output when you run the code.
-      #PIN CONNECTIONS
-      #AO - 12
-      #VCC - 3.3V
-      #GND - GND
- 
-   COPYRIGHT: BharatPi @MIT license for usage on Bharat Pi boards
+   #PIN CONNECTIONS
+   AO - 12
+   VCC - 3.3V
+   GND - GND
+
+   REVISION HISTORY TABLE:
+   ------------------------------------------
+   Date      | Firmware Version | Comments
+   ------------------------------------------
+   25/03/2024 -    0.1.0       -    Initial release of sound sensor sample script to read the data(Used PyCraft IDE version - 1.1).
+   06/06/2024 -    0.1.1       -    Added the altered code for Thonny ide(Used Thonny IDE version - 4.1.4).
+
  *************************************************************************/
-
-
-
+                                                          
 from machine import ADC
 import time
 
