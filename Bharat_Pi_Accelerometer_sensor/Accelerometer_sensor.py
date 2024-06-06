4b6ca4bc26e40e7e2e14931a555e95d370a9832c
Snehaaa-C <167289991+Snehaaa-C@users.noreply.github.com>
2024-06-06 12:24:38 +0530

    Update Accelerometer_sensor.py

--- Bharat_Pi_Accelerometer_sensor/Accelerometer_sensor.py
+++ Bharat_Pi_Accelerometer_sensor/Accelerometer_sensor.py
@@ -1,12 +1,21 @@
 /*************************************************************************
-   PROJECT: Bharat Pi Accelerometer Sample Code
+   PROJECT NAME:  Bharat Pi Accelerometer Sample Code
    AUTHOR: Bharat Pi
-
-   DESC: This script will give you the sample testing of accelerometer sensor,
-   which gives you the acceleration values.
- 
+   CREATED DATE: 25/03/2024
    COPYRIGHT: BharatPi @MIT license for usage on Bharat Pi boards
- *************************************************************************/
+   VERSION: 0.1.1
+
+   DESCRIPTION: This script will give you the sample testing of accelerometer sensor,
+   which gives you the acceleration values.
+                                   
+   REVISION HISTORY TABLE:
+   ------------------------------------------
+   Date      | Firmware Version | Comments
+   ------------------------------------------
+   25/03/2024 -    0.1.0       -    Initial release of accelerometer sensor sample script to read the data(Used PyCraft IDE version - 1.1).
+   06/06/2024 -    0.1.1       -    Added the altered code for Thonny ide(Used Thonny IDE version - 4.1.4).
+
+ ************************************************************************/
                                    
 from machine import Pin, SoftI2C
 import time
