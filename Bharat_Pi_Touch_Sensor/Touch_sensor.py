5bfd43e9d40e10d13a938862571e5420c96848d5
Snehaaa-C <167289991+Snehaaa-C@users.noreply.github.com>
2024-06-06 14:13:19 +0530

    Update Touch_sensor.py

--- Bharat_Pi_Touch_Sensor/Touch_sensor.py
+++ Bharat_Pi_Touch_Sensor/Touch_sensor.py
@@ -1,20 +1,30 @@
 /*************************************************************************
-   PROJECT: Bharat Pi Touch Sensor Sample Code
+   
+   PROJECT NAME: Bharat Pi Touch Sensor Sample Code
    AUTHOR: Bharat Pi
+   CREATED DATE: 25/03/2024
+   COPYRIGHT: BharatPi @MIT license for usage on Bharat Pi boards
+   VERSION: 0.1.1
 
-   DESC: This script will give you the sample testing of touch sensor,
+   DESCRIPTION: This script will give you the sample testing of touch sensor,
    which will detects/sense the touch and
    if touch detected prints the message (touch detected),
    if not then prints the message (no touch detected) when you run the code.
-     #PIN CONNECTIONS
-     #GND - GND
-     #VCC - 3.3V
-     #SIG - 32
+   #PIN CONNECTIONS
+   GND - GND
+   VCC - 3.3V
+   SIG - 32
 
-   COPYRIGHT: BharatPi @MIT license for usage on Bharat Pi boards
+   REVISION HISTORY TABLE:
+   ------------------------------------------
+   Date      | Firmware Version | Comments
+   ------------------------------------------
+   25/03/2024 -    0.1.0       -    Initial release of touch sensor sample script to read the data(Used PyCraft IDE version - 1.1).
+   06/06/2024 -    0.1.1       -    Added the altered code for Thonny ide(Used Thonny IDE version - 4.1.4).
+
  *************************************************************************/
 
-
+                                                          
 from machine import Pin
 import time
 
