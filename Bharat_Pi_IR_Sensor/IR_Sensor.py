15e17c3deb2ca747bd0c60bea79efe16605aa8aa
Snehaaa-C <167289991+Snehaaa-C@users.noreply.github.com>
2024-06-06 12:17:32 +0530

    Update IR_Sensor.py

--- Bharat_Pi_IR_Sensor/IR_Sensor.py
+++ Bharat_Pi_IR_Sensor/IR_Sensor.py
@@ -1,13 +1,26 @@
 /*************************************************************************
-   PROJECT: Bharat Pi IR sensor Sample Code
+   PROJECT NAME: Bharat Pi IR sensor Sample Code
    AUTHOR: Bharat Pi
+   CREATED DATE: 25/03/2024
+   COPYRIGHT: BharatPi @MIT license for usage on Bharat Pi boards
+   VERSION: 0.1.1
 
-   DESC: This script will give you the sample testing of IR sensor,
+   DESCRIPTION: This script will give you the sample testing of IR sensor,
    which will do the LED light turn on when any Obstacle detected near the sensor 
    and turn off the led when Obstacle cleared 
    and it will give the respective message for every three second when you run the code.
- 
-   COPYRIGHT: BharatPi @MIT license for usage on Bharat Pi boards
+   #PIN CONNECTIONS:
+   OUT   -   23
+   GND   -   GND
+   VCC   -   3.3V
+
+   REVISION HISTORY TABLE:
+   ------------------------------------------
+   Date      | Firmware Version | Comments
+   ------------------------------------------
+   25/03/2024 -    0.1.0       -    Initial release of IR sensor sample script to read the data(Used PyCraft IDE version - 1.1).
+   06/06/2024 -    0.1.1       -    Added the altered code for Thonny ide(Used Thonny IDE version - 4.1.4).
+
  *************************************************************************/
 
 
